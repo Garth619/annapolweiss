303cd8351af5b3f203cfdabf0c5590d72b6c0625
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-02-14 21:13:15 -0800

    Push

--- wp-content/themes/anapolweiss/page-main.php
+++ wp-content/themes/anapolweiss/page-main.php
@@ -174,16 +174,47 @@ get_header(); ?>
 
 				<p>Cras venenatis, massa et imperdiet ultrices, ante nisl imperdiet magna, nec mollis ante nisl vitae eros. Curabitur consequat facilisis lectus a consequat. Aliquam erat volutpat. Sed est lacus, egestas a risus posuere, pharetra elementum risus. Maecenas ullamcorper tortor ac eros adipiscing, ac commodo purus varius. Curabitur placerat nec leo quis porta. Nam pharetra, dui semper imperdiet dictum, ipsum augue auctor nibh, vel porta velit diam eget neque. Nullam fermentum mauris eu massa iaculis, et gravida dolor tincidunt. Duis et pellentesque augue, quis porttitor ligula. Nunc sit amet sem in dolor euismod rutrum.</p>
 				
-				<div class="quote_wrapper"></div><!-- quote_wrapper -->
+				<div class="quote_wrapper">
+					
+					<div class="greyline"></div><!-- greyline -->
+					
+					<span>Cras nec elit ac augue pretium commodo non id mi. Proin eleifend consequat purus ac aliquam. Etiam arcu leo, pellentesque ac pharetra et, ultrices bibendum massa. Cras arcu metus, dictum in erat id, semper condimentum ligula. </span>
+					
+									
+					<div class="horizontal_greyline"></div>
+					
+				</div><!-- quote_wrapper -->
+				
+					<p>Suspendisse potenti. Quisque at consequat urna. Nam lobortis elementum sapien, a blandit tortor placerat ac. Nam porttitor consectetur dui, sed rhoncus nisl porttitor non. Curabitur eu ornare sapien. Praesent non adipiscing nisi. Aenean adipiscing risus eget feugiat pharetra. Proin malesuada sem lectus, et facilisis ante condimentum a. Phasellus nec tellus tempor, adipiscing orci sit amet, dapibus magna. Phasellus ultricies lacus vitae pharetra pharetra. Nulla eleifend, risus non semper lobortis, massa eros molestie nulla, eget adipiscing felis neque ultrices justo. Quisque facilisis magna ut erat laoreet adipiscing. Pellentesque non suscipit nisi, vel ultricies tellus. Etiam vel porttitor turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tempus orci lacus, sit amet scelerisque ligula vestibulum vitae. Mauris faucibus, urna a pharetra venenatis, velit diam tempor sem, nec ornare felis diam sit amet turpis. Aliquam eget ullamcorper magna. Nam id convallis turpis. Mauris porta diam augue. Sed sit amet nisl nec arcu mollis faucibus tempor id ante. Curabitur commodo vestibulum nulla at lacinia.</p>
+
+					<p>Cras venenatis, massa et imperdiet ultrices, ante nisl imperdiet magna, nec mollis ante nisl vitae eros. Curabitur consequat facilisis lectus a consequat. Aliquam erat volutpat. Sed est lacus, egestas a risus posuere, pharetra elementum risus. Maecenas ullamcorper tortor ac eros adipiscing, ac commodo purus varius. Curabitur placerat nec</p>
+
 				
 			</div><!-- content -->
 			
 			
 		</div><!-- about_right -->
-	
+		
+			
 	</div><!-- about_inner -->
 	
+	
+
+	
 </section><!-- about -->
+
+
+<section class="featured_cases">
+	
+	
+	<div class="wavy">
+			
+<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1968.63 224.08"><defs><style>.cls-1{fill:#c4c8c9;}</style></defs><title>content_waves_1</title><path class="cls-1" d="M1968.41,224.08a.22.22,0,0,0,.19-.14.34.34,0,0,0-.08-.4c-45.48-34.78-97.19-61.73-158.07-82.38-60-20.35-131.32-35.38-218.06-45.95-132.5-16.15-260.95,22.24-374.28,56.11-33.28,9.95-64.71,19.34-95.17,27.1-71.39,18.2-176.68,28.58-281.65,27.73C783.7,205.71,729,202,678.6,195,623,187.34,574.21,175.93,533.67,161.1c-41.64-15.23-88.49-27-139.26-35.11a948.28,948.28,0,0,0-148.5-11.7c-49.71,0-96.14,3.94-138,11.7C64.38,134.07,28.12,145.89.14,161.12a.32.32,0,0,0-.12.38c0,.15.18.22.29.16,28-15.21,64.18-27,107.66-35.09,41.84-7.76,88.25-11.7,137.94-11.7a947.94,947.94,0,0,1,148.45,11.7c50.74,8.07,97.57,19.88,139.19,35.1,40.57,14.83,89.35,26.25,145,33.92,50.37,6.95,105.13,10.7,162.75,11.16,3.93,0,7.87,0,11.8,0,101,0,201.19-10.26,269.94-27.79,30.46-7.77,61.9-17.16,95.18-27.11,113.3-33.86,241.7-72.24,374.13-56.1,86.71,10.57,158,25.59,218,45.93,60.84,20.64,112.51,47.56,158,82.32A.18.18,0,0,0,1968.41,224.08Z" transform="translate(0 0)"/><circle class="cls-1" cx="1601.66" cy="96.97" r="1.59"/><circle class="cls-1" cx="1188.33" cy="160.97" r="1.59"/><circle class="cls-1" cx="545" cy="165.38" r="1.59"/><circle class="cls-1" cx="247.66" cy="114.72" r="1.59"/><path class="cls-1" d="M.22,132.67a.22.22,0,0,1-.19-.14.34.34,0,0,1,.08-.4C45.59,97.35,97.3,70.4,158.18,49.76c60-20.35,131.32-35.38,218.06-45.95C508.74-12.34,637.18,26,750.51,59.92c33.28,9.95,64.71,19.34,95.17,27.1,71.39,18.2,176.68,28.58,281.65,27.73,57.6-.46,112.34-4.21,162.7-11.16,55.63-7.67,104.39-19.08,144.94-33.91,41.64-15.23,88.49-27,139.26-35.11a948.28,948.28,0,0,1,148.5-11.7c49.71,0,96.14,3.94,138,11.7,43.52,8.08,79.79,19.89,107.77,35.13a.32.32,0,0,1,.12.38c0,.15-.18.22-.29.16-28-15.21-64.18-27-107.66-35.09-41.84-7.76-88.25-11.7-137.94-11.7a947.94,947.94,0,0,0-148.45,11.7c-50.74,8.07-97.57,19.88-139.19,35.1-40.57,14.83-89.35,26.25-145,33.92-50.37,6.95-105.13,10.7-162.75,11.16-3.93,0-7.87,0-11.8,0-101,0-201.19-10.26-269.94-27.79-30.46-7.77-61.9-17.16-95.18-27.11-113.3-33.86-241.7-72.24-374.13-56.1C289.57,15,218.26,30,158.29,50.31,97.45,71,45.78,97.88.34,132.63A.18.18,0,0,1,.22,132.67Z" transform="translate(0 0)"/><circle class="cls-1" cx="366.97" cy="5.56" r="1.59"/><circle class="cls-1" cx="780.3" cy="69.56" r="1.59"/><circle class="cls-1" cx="1423.64" cy="73.97" r="1.59"/><circle class="cls-1" cx="1720.97" cy="23.3" r="1.59"/></svg>
+			
+		</div><!-- wavy -->
+	
+	
+</section><!-- featured_cases -->
 
 
 
