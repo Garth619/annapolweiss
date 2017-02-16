03a57b5c0b3f03e9089fc160dba6a7353fc9a548
Garrett Cullen <garrettwcullen@gmail.com>
2017-02-16 14:16:26 -0800

    push

--- wp-content/themes/anapolweiss/footer.php
+++ wp-content/themes/anapolweiss/footer.php
@@ -1,4 +1,30 @@
 
+
+
+<section class="get_started">
+	
+	
+	<div class="get_started_left">
+		
+		<div class="get_started_title_wrapper">
+			
+			<span class="sub_header">Free Case Evaluation</span><!-- sub_header -->
+			<span class="large_header">get started today</span><!-- large_header -->
+			<span class="info_required">*information required</span><!-- large_header -->
+			<span class="get_started_orangeline"></span><!-- get_started_orangeline -->
+			
+		</div><!-- get_started_title_wrapper -->
+		
+		
+		
+	</div><!-- get_started_left -->
+	
+	<div class="get_started_right"></div><!-- get_started_right -->
+	
+	
+</section><!-- get_started -->
+
+
 	
 <?php wp_footer(); ?>
 <script type="text/javascript" src="<?php bloginfo('template_directory');?>/js/slick/slick/slick.min.js"></script>
