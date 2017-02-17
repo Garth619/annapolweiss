423a33261ae4188c78fa21f06f836698cdb4a8f2
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-02-16 22:28:30 -0800

    push

--- wp-content/themes/anapolweiss/footer.php
+++ wp-content/themes/anapolweiss/footer.php
@@ -58,7 +58,7 @@
 						
 						<div class="single_location">
 						
-							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600 Philadelphia, PA 19103</a><!-- address -->
+							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600<br/>Philadelphia, PA 19103</a><!-- address -->
 						
 							<a class="location_tel" href="tel:21573511340">(215) 735-11340</a>
 					
@@ -66,34 +66,9 @@
 						
 						<div class="single_location">
 						
-							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600 Philadelphia, PA 19103</a><!-- address -->
+							<a class="address" href="" target="_blank">4807 Jonestown Rd, Suite 148<br/>Harrisburg, PA 171093</a><!-- address -->
 						
-							<a class="location_tel" href="tel:21573511340">(215) 735-11340</a>
-					
-						</div><!-- single_location -->
-					
-					
-					</div><!-- location_col -->
-					
-					<div class="location_col">
-					
-					<span class="state">Pennsylvania</span><!-- state -->
-						
-						<span class="location_line"></span><!-- location_line -->
-						
-						<div class="single_location">
-						
-							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600 Philadelphia, PA 19103</a><!-- address -->
-						
-							<a class="location_tel" href="tel:21573511340">(215) 735-11340</a>
-					
-						</div><!-- single_location -->
-						
-						<div class="single_location">
-						
-							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600 Philadelphia, PA 19103</a><!-- address -->
-						
-							<a class="location_tel" href="tel:21573511340">(215) 735-11340</a>
+							<a class="location_tel" href="tel:7179013500">(717) 901-3500</a>
 					
 						</div><!-- single_location -->
 					
@@ -102,15 +77,34 @@
 					
 					<div class="location_col">
 					
-					<span class="state">Pennsylvania</span><!-- state -->
+					<span class="state">New Jersey</span><!-- state -->
 						
 						<span class="location_line"></span><!-- location_line -->
 						
 						<div class="single_location">
 						
-							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600 Philadelphia, PA 19103</a><!-- address -->
+							<a class="address" href="" target="_blank">1040 N. Kings Highway<br/>Cherry Hill, NJ 080343</a><!-- address -->
 						
-							<a class="location_tel" href="tel:21573511340">(215) 735-11340</a>
+							<a class="location_tel" href="tel:8564279229">(856) 427-9229</a>
+					
+						</div><!-- single_location -->
+						
+					
+					
+					
+					</div><!-- location_col -->
+					
+					<div class="location_col">
+					
+					<span class="state">Arizona</span><!-- state -->
+						
+						<span class="location_line"></span><!-- location_line -->
+						
+						<div class="single_location">
+						
+							<a class="address" href="" target="_blank">8700 E. Vista Bonita Dr, Suite 268<br/>Scottsdale, AZ 852553</a><!-- address -->
+						
+							<a class="location_tel" href="tel:4805154745">(480) 515-4745</a>
 					
 						</div><!-- single_location -->
 						
@@ -135,6 +129,18 @@
 	
 	<div class="social_links">
 		
+		<div class="social_inner">
+		
+			<span class="social_title">social</span><!-- socail_title -->
+		
+			<ul>
+				<li><a href="" target="_blank">Facebook</a></li>
+				<li><a href="" target="_blank">Google+</a></li>
+				<li><a href="" target="_blank">Linkedin</a></li>
+				<li><a href="" target="_blank">Twitter</a></li>
+			</ul>
+		
+		</div><!-- social_inner -->
 		
 		
 	</div><!-- social_links -->
