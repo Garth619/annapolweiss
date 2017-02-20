fee21a91d895d40beb181b9bd8b7f32870296a39
Garrett Cullen <garrettwcullen@gmail.com>
2017-02-20 14:35:41 -0800

    push

---
+++ wp-content/themes/anapolweiss/banner.php
@@ -0,0 +1,14 @@
+<div class="inner_banner_wrapper">
+	
+	
+	<div class="inner_banner_content_wrapper">
+		
+		<span class="sub_header">Good People</span><!-- subheader -->
+		<span class="large_header">Great Lawyers</span><!-- large_header -->
+		
+		<a class="free" href="#free_consultation">Start Your Free Consultation</a>
+		
+	</div><!-- inner_banner_content_wrapper -->
+	
+	
+</div><!-- inner_banner_wrapper -->
--- wp-content/themes/anapolweiss/page.php
+++ wp-content/themes/anapolweiss/page.php
@@ -5,9 +5,59 @@
 
 get_header(); ?>
 
-<?php get_template_part( 'loop', 'page' ); ?>
+
+
+<?php include('banner.php');?>
 
 
 
-<?php get_sidebar(); ?>
+<div class="inner_main_wrapper">
+	
+	
+	<div class="inner_page_content">
+		
+		<h1>Inner Page Styling</h1>
+		
+		<div class="content">
+			
+			<p>Millions of people are hurt each year by the careless and negligent behavior of others. Not only are these injuries physically painful, they are emotionally draining. Instead of being able to focus solely on recovery, victims and their families must worry about insurance claims, lost wages, and costly medical bills.</p>
+
+<p>Anapol Weiss is a nationally recognized personal injury law firm with a winning track record. Our attorneys fight for maximum compensation for our clients in a wide range of personal injury situations including:</p>
+				<ul>
+        <li>Automobile accidents</li>
+        <li>Burn injuries</li>
+        <li>Construction accidents</li>
+        <li>Slips and falls</li>
+        <li>Spinal cord injuries</li>
+        <li>Traumatic brain injuries</li>
+        <li>Wrongful death</li>
+				</ul>
+
+<p>We work closely with a superior team of medical and technical experts when building our cases. The firm’s tireless efforts ensure no stone is left unturned, maximizing recovery for our clients.</p>
+
+
+<h2>H2 Styling </h2>
+
+
+			
+			
+		</div><!-- content -->
+		
+	</div><!-- inner_page_content -->
+	
+	<?php get_sidebar(); ?>
+	
+	
+</div><!-- inner_main_container -->
+
+
+
+
+
+
+<?php // get_template_part( 'loop', 'page' ); ?>
+
+
+
+
 <?php get_footer(); ?>
