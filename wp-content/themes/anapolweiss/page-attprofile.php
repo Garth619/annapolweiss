c5eb0ccd159cb3d4765d26952471611c0dc94079
Garrett Cullen <garrettwcullen@gmail.com>
2017-02-21 10:54:25 -0800

    push

---
+++ wp-content/themes/anapolweiss/page-attprofile.php
@@ -0,0 +1,68 @@
+<?php
+/**
+* Template Name: Attorney Profile
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+<div class="inner_page_orangeline">
+	
+	<span class="line"></span>
+	
+</div><!-- inner_page_orangeline -->
+
+<div class="inner_main_wrapper">
+	
+	
+	
+	<div class="inner_page_content">
+		
+		<h1>Tom Anapol</h1>
+		<span class="">shareholder</span>
+		
+		<div class="content">
+			
+			<h2>Leader in national medical device litigations</h2>
+			
+			<p>Thomas Anapol has served in a variety of leadership capacities in state and multidistrict litigation (MDL) Plaintiff Steering Committees (PSC). His roles include:</p>
+
+        <ul>
+        <li>Co-Lead Counsel in the Biomet metal-on-metal hip implant litigation</li>
+         <li>Member of the PSC in the Stryker Rejuvenate & ABG II NJ State coordinated litigation</li>
+        </ul>
+
+				<p>As a leader in these litigations, Tom was instrumental in negotiating both the Biomet and Stryker global settlements. He also currently serves on the DePuy Pinnacle metal-on-metal hip implant MDL and on four Pelvic Mesh MDL PSCs.	</p>	
+				
+				<h2>Accomplished legal background</h2>
+				
+				
+				<p>Tom has served as an officer for the Pennsylvania Association for Justice (PAJ) for several years. He was elected Treasurer for 2012-2013, Vice President in 2014, President-Elect in 2015 and President in 2016. Tom also currently serves on the Board of Governors for the American Association for Justice (AAJ). Within AAJ, Tom serves as Co-Chair of AAJ’s DePuy Metal on Metal Litigation Group. Since 2012, he has also served on the AAJ Sections and Litigation Group Coordination Committee (SLGCC).</p>
+
+				<p>In addition to his legal work in pharmaceutical and medical device litigation, Tom concentrates his practice on mesothelioma and other catastrophic injury-related civil litigation. He has been named one of the Top 100 Lawyers in Pennsylvania and Philadelphia since 2012, and he has been included in Pennsylvania Super Lawyers every year since 2006.</p>
+
+				<p>Tom earned his law degree from Widener University School of Law in 1991. He is admitted to practice in Pennsylvania, New Jersey and various federal courts across the country. Tom routinely lectures about Mass Torts at AAJ, PAJ and other legal seminars across the country.</p>
+			
+		</div><!-- content -->
+		
+	</div><!-- inner_page_content -->
+	
+	<?php get_sidebar(); ?>
+	
+	
+</div><!-- inner_main_container -->
+
+
+
+
+
+
+<?php // get_template_part( 'loop', 'page' ); ?>
+
+
+
+
+<?php get_footer(); ?>
