786ec86c5eefcd55c34891ac3caace30b3300f55
Garrett Cullen <garrettwcullen@gmail.com>
2017-02-21 14:48:35 -0800

    push

---
+++ wp-content/themes/anapolweiss/page-aboutus.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: About Us
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+<div class="inner_page_orangeline">
+	
+	<span class="line"></span>
+	
+</div><!-- inner_page_orangeline -->
+
+<div class="inner_main_wrapper_no_sidebar">
+	
+	
+	
+	<div class="inner_page_content_about">
+		
+		<h1>About Us</h1>
+		
+		<div class="content">
+			
+			<p class="intro">At Anapol Weiss, we are committed to providing the best counsel, advocacy and service to our clients.  We are committed to understanding your unique needs and serving as a resource and partner.  We are dedicated to obtaining justice for those who have been injured or wronged while maintaining the highest standards of personal and professional integrity.  We promise to be accessible, efficient, responsive and transparent and will continue our long tradition of working hard for the benefit of you, our client.</p>
+			
+			
+			<h2>Who We are</h2>
+			<span class="aboutus_greyline"></span><!-- greyline -->
+			
+			<p><strong>For more than 35 years, the attorneys at Anapol Weiss have had this guiding principle:</strong><br/>
+We passionately believe it is our responsibility to stand up for those who have been harmed by someone else’s negligence, whether that someone is a single individual or a multibillion-dollar corporation.</p>
+
+			<h2>What drives our firm</h2>
+			<span class="aboutus_greyline"></span><!-- greyline -->
+			
+			<p>Our commitment to giving every victim a voice is at the heart of all we do. We are dedicated to obtaining justice for those who have been injured or wronged, speaking out for them, and getting their voices heard.</p>
+			
+		</div><!-- content -->
+		
+	</div><!-- inner_page_content_about -->
+	
+	
+	<div class="about_orange_box">
+		
+		
+		
+	</div><!-- about_orange_box -->
+	
+	
+	
+</div><!-- inner_main_container -->
+
+
+
+
+
+
+<?php // get_template_part( 'loop', 'page' ); ?>
+
+
+
+
+<?php get_footer(); ?>
