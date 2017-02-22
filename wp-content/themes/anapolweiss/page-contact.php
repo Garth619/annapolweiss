dd0bf96cc8c4c0e7d6fcb50e5c35ca392521c030
Garrett Cullen <garrettwcullen@gmail.com>
2017-02-22 10:02:18 -0800

    push

---
+++ wp-content/themes/anapolweiss/page-contact.php
@@ -0,0 +1,147 @@
+<?php
+/**
+* Template Name: Contact
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+<div class="inner_page_orangeline">
+	
+	<span class="line"></span>
+	
+</div><!-- inner_page_orangeline -->
+
+<div class="inner_main_wrapper">
+	
+	
+	
+	<div class="inner_page_content">
+		
+		<h1>Contact</h1>
+		
+		
+		
+		<div class="locations_outer_wrapper">
+		
+		
+		<div class="locations_wrapper">
+		
+		<span class="location_title">locations</span><!-- location_title -->
+		
+		<div class="location_slider">
+			
+			<div class="location_slider_inner">
+				
+				<div class="location_col">
+					
+					<span class="state">Pennsylvania</span><!-- state -->
+						
+						<span class="location_line"></span><!-- location_line -->
+						
+						<div class="single_location">
+						
+							<a class="address" href="" target="_blank">One Logan Square 130 N. 18th St. Ste. 1600<br/>Philadelphia, PA 19103</a><!-- address -->
+							<a href="" class="get_directions" target="_blank">Get Directions</a>
+						
+							<a class="location_tel" href="tel:21573511340">(215) 735-11340</a>
+					
+						</div><!-- single_location -->
+						
+						<div class="single_location">
+						
+							<a class="address" href="" target="_blank">4807 Jonestown Rd, Suite 148<br/>Harrisburg, PA 171093</a><!-- address -->
+							<a href="" class="get_directions" target="_blank">Get Directions</a>
+						
+							<a class="location_tel" href="tel:7179013500">(717) 901-3500</a>
+					
+						</div><!-- single_location -->
+					
+					
+					</div><!-- location_col -->
+					
+					<div class="tablet">
+					
+					<div class="location_col">
+					
+					<span class="state">New Jersey</span><!-- state -->
+						
+						<span class="location_line"></span><!-- location_line -->
+						
+						<div class="single_location">
+						
+							<a class="address" href="" target="_blank">1040 N. Kings Highway<br/>Cherry Hill, NJ 080343</a><!-- address -->
+							<a href="" class="get_directions" target="_blank">Get Directions</a>
+						
+							<a class="location_tel" href="tel:8564279229">(856) 427-9229</a>
+					
+						</div><!-- single_location -->
+						
+					
+					
+					
+					</div><!-- location_col -->
+					
+					<div class="location_col">
+					
+					<span class="state">Arizona</span><!-- state -->
+						
+						<span class="location_line"></span><!-- location_line -->
+						
+						<div class="single_location">
+						
+							<a class="address" href="" target="_blank">8700 E. Vista Bonita Dr,<br class="tablet_break"/> Suite 268<br/>Scottsdale, AZ 852553</a><!-- address -->
+							<a href="" class="get_directions" target="_blank">Get Directions</a>
+						
+							<a class="location_tel" href="tel:4805154745">(480) 515-4745</a>
+					
+						</div><!-- single_location -->
+						
+						</div><!-- tablet -->
+					
+					
+					</div><!-- location_col -->
+				
+			</div><!-- location_slider_inner -->
+			
+			
+			
+		</div><!-- location_slider -->
+		
+	</div><!-- locations -->
+	
+	<div class="social_links">
+		
+		<div class="social_inner">
+		
+			<span class="social_title">social</span><!-- socail_title -->
+		
+			<ul>
+				<li><a href="" target="_blank">Facebook</a></li>
+				<li><a href="" target="_blank">Google+</a></li>
+				<li><a href="" target="_blank">Linkedin</a></li>
+				<li><a href="" target="_blank">Twitter</a></li>
+			</ul>
+		
+		</div><!-- social_inner -->
+		
+		
+	</div><!-- social_links -->
+	
+	
+	</div><!-- locations_outer_wrapper -->
+
+				
+				
+	</div><!-- inner_page_content -->
+	
+
+	
+	
+</div><!-- inner_main_container -->
+
+
+<?php get_footer(); ?>
