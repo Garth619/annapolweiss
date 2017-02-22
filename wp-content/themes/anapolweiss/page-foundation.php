8b60409a3de220946897dbc27bbdc6a37040a47f
Garrett Cullen <garrettwcullen@gmail.com>
2017-02-22 14:05:59 -0800

    push

---
+++ wp-content/themes/anapolweiss/page-foundation.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * Template Name: Foundation
+ */
+
+get_header(); ?>
+
+
+<div class="inner_banner_foundation">
+	
+	<div class="inner_content_foundation">
+	
+		<span class="sub_header">Our Cases Become</span><!-- sub_header -->
+		<span class="large_header">Our Causes</span><!-- large_header -->
+	
+	</div><!-- inner_content_foundation -->
+	
+	
+	
+</div><!-- inner_banner_foundation -->
+
+
+<div class="inner_page_orangeline">
+	
+	<span class="line"></span>
+	
+</div><!-- inner_page_orangeline -->
+
+<div class="inner_main_wrapper_no_sidebar">
+	
+	
+	
+	<div class="inner_page_content_about">
+		
+		<h1>Anapol Weiss Foundation</h1>
+		
+		<div class="content">
+			
+			<p class="intro">Compassionate representation necessarily involves the willingness to listen to our clients and try to understand their experience. We wanted to not only help our clients, but others as well.</p>
+
+<p class="intro">The Anapol Weiss Foundation supports people and organizations that promote community health and safety. Funded by a portion of fees from successful cases, the Foundation has donated hundreds of thousands of dollars to non-profits that serve people with injuries similar to those of the firm’s clients.</p>
+			
+			
+			<h2>the burn foundation</h2>
+			<span class="aboutus_greyline"></span><!-- greyline -->
+			
+			<p>The Anapol Weiss Foundation dedicated its first grant to The Burn Foundation, a non-profit that provides education and support to the burn care community as well as burn survivors and their families throughout the greater Philadelphia region. The Burn Foundation used the donation to launch a new family mentorship program for burn survivors and their families.</p>
+
+			<h2>What drives our firm</h2>
+			<span class="aboutus_greyline"></span><!-- greyline -->
+			
+			<p>Our commitment to giving every victim a voice is at the heart of all we do. We are dedicated to obtaining justice for those who have been injured or wronged, speaking out for them, and getting their voices heard.</p>
+			
+			
+			<h2>magee rehabilitation hospital</h2>
+			<span class="aboutus_greyline"></span><!-- greyline -->
+			
+			<p>The Anapol Weiss Foundation funded a new center at Magee Rehabilitation Hospital to provide teen inpatients with a safe and fun space all their own. The Anapol Weiss Foundation Adolescent Entertainment Center will provide a solution to the adolescent patients’ unmet needs by providing a setting for patients and their visitors to enjoy each other’s company.</p>
+
+<p>The Foundation began a fund for the Caren Sydnor Locomotor Training Fund at Magee Rehabilitation Hospital Foundation, which provides training to spinal cord injury patients when their insurance coverage runs out.</p>
+
+<p>The Anapol Weiss Foundation also supports the Magee Rehabilitation Hospital Amputee Group. In 2010, the Anapol Foundation established the Amputee Support Fund, which aids amputees in obtaining necessary items such wheelchairs, home modifications and driving lessons. The Foundation also made a donation in 2013 to allow amputee Saineh Barclay attend an Amputee Coalition Conference in California.</p>
+
+<p>“In everything our firm does, we’re striving to help victims affected by disabling injuries,” said firm partner Joel Feldman. “We’re proud to partner with Magee in this effort to improve the lives of amputees in our region.”</p>
+			
+		</div><!-- content -->
+		
+	</div><!-- inner_page_content_about -->
+	
+	
+	<div class="about_orange_box">
+		
+		<div class="inner_about_slider_wrapper">
+			
+			<div class="orange_caption">Physical therapist Curry Durborow received the 2012 Anapol Advocate Award for her role in traumatic brain injury research and education projects. The Foundation presented a grant to the Brain Injury Association of Pennsylvania on her behalf.</div><!-- orange_caption -->
+			
+			<div class="inner_about_slider">
+				
+				
+				<div class="inner_about_slides">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/foundation_slide_1.jpg"/>
+					
+				</div><!-- inner_about_slides -->
+				
+				
+				<div class="inner_about_slides">
+					
+					<img src="<?php bloginfo('template_directory');?>/images/foundation_slide_2.jpg"/>
+					
+				</div><!-- inner_about_slides -->
+				
+								
+				
+			</div><!-- inner_about_slider -->
+			
+			<div class="inner_about_caption">
+				
+				<span class="inner_about_caption_title">the anapol advocate award</span><!-- inner_about_caption_title -->
+				<span class="inner_about_line"></span><!-- inner_about_greyline -->
+				
+			</div><!-- inner_about_caption -->
+			
+			
+			<div class="inner_about_buttons">
+				
+				<div class="inner_about_back">
+					
+					<img class="inner_about_back_img" src="<?php bloginfo('template_directory');?>/images/content_left_arrow.png"/>
+					<img class="inner_about_back_hover_img" src="<?php bloginfo('template_directory');?>/images/content_left_arrow_orange.png"/>
+					
+				</div><!-- inner_about_back -->
+				
+				<div class="inner_about_counter_spacer">1/8</div><!-- inner_about_counter_spacer -->
+				
+				<div class="inner_about_next">
+					
+					<img class="inner_about_next_img" src="<?php bloginfo('template_directory');?>/images/content_right_arrow.png"/>
+					<img class="inner_about_next_hover_img" src="<?php bloginfo('template_directory');?>/images/content_right_arrow_orange.png"/>
+					
+				</div><!-- inner_about_back -->
+				
+			</div><!-- inner_about_buttons -->
+			
+			
+		</div><!-- inner_about_slider_wrapper -->
+		
+	</div><!-- about_orange_box -->
+	
+	
+	
+</div><!-- inner_main_container -->
+
+
+
+<?php get_footer(); ?>
