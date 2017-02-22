60602b429095cb807dca9ab1e9af3e1dc7cca71f
Garrett Cullen <garrettcullen@garretts-MacBook-Pro.local>
2017-02-21 22:08:22 -0800

    push

---
+++ wp-content/themes/anapolweiss/page-practiceareas.php
@@ -0,0 +1,79 @@
+<?php
+/**
+* Template Name: Practice Areas
+ */
+
+get_header(); ?>
+
+
+
+<?php include('banner.php');?>
+
+<div class="inner_page_orangeline">
+	
+	<span class="line"></span>
+	
+</div><!-- inner_page_orangeline -->
+
+<div class="inner_main_wrapper">
+	
+	
+	
+	<div class="inner_page_content">
+		
+		<h1>Practice Areas</h1>
+		
+		
+		<div class="practice_areas_list_wrapper">
+			
+			<ul>
+				
+				
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				<li><a href="">Automobile or Trucking Accidents</a></li>
+				
+				
+			</ul>
+			
+			
+		</div><!-- practice_areas_list_wrapper -->
+		
+		
+				
+	</div><!-- inner_page_content -->
+	
+
+	
+	
+</div><!-- inner_main_container -->
+
+
+<?php get_footer(); ?>
