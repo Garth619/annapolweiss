9c312fb283452f2adecd8177b29fcb2419061a9c
Garrett Cullen <garrettwcullen@gmail.com>
2017-03-01 15:51:55 -0800

    push

--- wp-content/themes/anapolweiss/page-videos.php
+++ wp-content/themes/anapolweiss/page-videos.php
@@ -23,7 +23,54 @@ get_header(); ?>
 		
 		<h1>Video Center</h1>
 		
+		<div class="video_wrapper">
+			
+			<div class="single_video_wrapper">
 				
+				<script src="//fast.wistia.com/embed/medias/ft8q2y0n8y.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_ft8q2y0n8y popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<div class="video_title">Taking Cases To Trail</div><!-- video_title -->
+				
+			</div><!-- single_video_wrapper -->
+			
+			
+			<div class="single_video_wrapper">
+				
+				<script src="//fast.wistia.com/embed/medias/ft8q2y0n8y.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_ft8q2y0n8y popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<div class="video_title">Video Title</div><!-- video_title -->
+				
+			</div><!-- single_video_wrapper -->
+			
+			
+			<div class="single_video_wrapper">
+				
+				<script src="//fast.wistia.com/embed/medias/ft8q2y0n8y.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_ft8q2y0n8y popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<div class="video_title">Video Title</div><!-- video_title -->
+				
+			</div><!-- single_video_wrapper -->
+			
+			
+			<div class="single_video_wrapper">
+				
+				<script src="//fast.wistia.com/embed/medias/ft8q2y0n8y.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_ft8q2y0n8y popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<div class="video_title">Video Title</div><!-- video_title -->
+				
+			</div><!-- single_video_wrapper -->
+			
+			
+			<div class="single_video_wrapper">
+				
+				<script src="//fast.wistia.com/embed/medias/ft8q2y0n8y.jsonp" async></script><script src="//fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><span class="wistia_embed wistia_async_ft8q2y0n8y popover=true popoverAnimateThumbnail=true videoFoam=true" style="display:inline-block;height:100%;width:100%">&nbsp;</span></div></div>
+				
+				<div class="video_title">Video Title</div><!-- video_title -->
+				
+			</div><!-- single_video_wrapper -->
+			
+			
+		</div><!-- video_wrapper -->
 
 				
 	</div><!-- inner_page_content -->
